0790e8344c506708548faab835f8b11e744d6256
Rick van Schijndel <rol3517@gmail.com>
2021-05-08 13:47:56 +0200

    add quite a bit of cross packages

--- release.nix
+++ release.nix
@@ -2,10 +2,28 @@
 let
   pkgs = (import <nixpkgs> {});
 in {
+  hello = pkgs.hello;
   aflplusplus = pkgs.aflplusplus;
-  #hello = pkgs.hello;
-  #rtl_433 = pkgs.rtl_433;
-  #jq = pkgs.jq;
-  #crossGtkDoc = pkgs.pkgsCross.aarch64-multiplatform.gtk-doc;
+  rtl_433 = pkgs.rtl_433;
+  jq = pkgs.jq;
+  nomacs = pkgs.nomacs;
+
+  # cross packages
+  crossBspwm = pkgs.pkgsCross.aarch64-multiplatform.bspwm;
+  crossCurseofwar = pkgs.pkgsCross.aarch64-multiplatform.curseofwar;
+  crossEd = pkgs.pkgsCross.aarch64-multiplatform.ed;
+  crossDhcpcd = pkgs.pkgsCross.aarch64-multiplatform.dhcpcd;
+  crossGtkDoc = pkgs.pkgsCross.aarch64-multiplatform.gtk-doc;
+  crossHtop = pkgs.pkgsCross.aarch64-multiplatform.htop;
+  crossMosquitto = pkgs.pkgsCross.aarch64-multiplatform.mosquitto;
+  crossNano = pkgs.pkgsCross.aarch64-multiplatform.nano;
+  crossNginx = pkgs.pkgsCross.aarch64-multiplatform.nginx;
+  crossOpenssh = pkgs.pkgsCross.aarch64-multiplatform.openssh;
+  crossRclone = pkgs.pkgsCross.aarch64-multiplatform.rclone;
+  crossRipgrep = pkgs.pkgsCross.aarch64-multiplatform.ripgrep;
+  crossRsync = pkgs.pkgsCross.aarch64-multiplatform.rsync;
+  crossRtl433 = pkgs.pkgsCross.aarch64-multiplatform.rtl_433;
+  crossSwaykbdd = pkgs.pkgsCross.aarch64-multiplatform.swaykbdd;
+  crossVim = pkgs.pkgsCross.aarch64-multiplatform.vim;
 }
 
