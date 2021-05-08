5bc86ceb03d20f9c15e2664aecee081c0ddfe036
Rick van Schijndel <rol3517@gmail.com>
2021-05-08 13:23:49 +0200

    update with aflplusplus, disable other packages for now

--- release.nix
+++ release.nix
@@ -2,9 +2,10 @@
 let
   pkgs = (import <nixpkgs> {});
 in {
-  hello = pkgs.hello;
-  rtl_433 = pkgs.rtl_433;
-  jq = pkgs.jq;
-  crossGtkDoc = pkgs.pkgsCross.aarch64-multiplatform.gtk-doc;
+  aflplusplus = pkgs.aflplusplus;
+  #hello = pkgs.hello;
+  #rtl_433 = pkgs.rtl_433;
+  #jq = pkgs.jq;
+  #crossGtkDoc = pkgs.pkgsCross.aarch64-multiplatform.gtk-doc;
 }
 
