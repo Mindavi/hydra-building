f23dddaa37c961f376ad2bb2d1878d2b195cd59c
Rick van Schijndel <rol3517@gmail.com>
2021-05-19 21:46:36 +0200

    add asciidoc, atk, gdk-pixbuf, gstreamer, librsvg, pango, st

--- release.nix
+++ release.nix
@@ -11,6 +11,8 @@ in {
   #nomacs = pkgs.nomacs; # disabled since it takes very long to build this
 
   # cross packages
+  crossAsciidoc = pkgs.pkgsCross.aarch64-multiplatform.asciidoc;
+  crossAtk = pkgs.pkgsCross.aarch64-multiplatform.atk;
   crossBspwm = pkgs.pkgsCross.aarch64-multiplatform.bspwm;
   crossBusybox = pkgs.pkgsCross.aarch64-multiplatform.busybox;
   crossCurseofwar = pkgs.pkgsCross.aarch64-multiplatform.curseofwar;
@@ -21,22 +23,27 @@ in {
   crossDhcpcd = pkgs.pkgsCross.aarch64-multiplatform.dhcpcd;
   crossFile = pkgs.pkgsCross.aarch64-multiplatform.file;
   crossFzf = pkgs.pkgsCross.aarch64-multiplatform.fzf;
+  crossGdkPixbuf = pkgs.pkgsCross.aarch64-multiplatform.gdk-pixbuf;
   crossGit = pkgs.pkgsCross.aarch64-multiplatform.git;
+  crossGstreamer = pkgs.pkgsCross.aarch64-multiplatform.gstreamer;
   crossGtkDoc = pkgs.pkgsCross.aarch64-multiplatform.gtk-doc;
   crossHarfbuzz = pkgs.pkgsCross.aarch64-multiplatform.harfbuzz;
   crossHtop = pkgs.pkgsCross.aarch64-multiplatform.htop;
   crossJq = pkgs.pkgsCross.aarch64-multiplatform.jq;
+  crossLibrsvg = pkgs.pkgsCross.aarch64-multiplatform.librsvg;
   crossLibsoup = pkgs.pkgsCross.aarch64-multiplatform.libsoup;
   crossLibwacom = pkgs.pkgsCross.aarch64-multiplatform.libwacom;
   crossMosquitto = pkgs.pkgsCross.aarch64-multiplatform.mosquitto;
   crossNano = pkgs.pkgsCross.aarch64-multiplatform.nano;
   crossNginx = pkgs.pkgsCross.aarch64-multiplatform.nginx;
   crossOpenssh = pkgs.pkgsCross.aarch64-multiplatform.openssh;
+  crossPango = pkgs.pkgsCross.aarch64-multiplatform.pango;
   crossPatch = pkgs.pkgsCross.aarch64-multiplatform.patch;
   #crossRclone = pkgs.pkgsCross.aarch64-multiplatform.rclone; # go build times out
   crossRipgrep = pkgs.pkgsCross.aarch64-multiplatform.ripgrep;
   crossRsync = pkgs.pkgsCross.aarch64-multiplatform.rsync;
   crossRtl433 = pkgs.pkgsCross.aarch64-multiplatform.rtl_433;
+  crossSt = pkgs.pkgsCross.aarch64-multiplatform.st;
   crossSway= pkgs.pkgsCross.aarch64-multiplatform.sway;
   crossSwaykbdd = pkgs.pkgsCross.aarch64-multiplatform.swaykbdd;
   crossVim = pkgs.pkgsCross.aarch64-multiplatform.vim;
