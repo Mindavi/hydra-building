d587b47c8af0b92c6188cb0cfd8fa25b4ef70aca
Rick van Schijndel <rol3517@gmail.com>
2021-12-04 17:24:25 +0100

    overlay: extract to separate file for easier reuse

--- release.nix
+++ release.nix
@@ -2,26 +2,7 @@
 let
   pkgs = (import <nixpkgs> {
     config.contentAddressedByDefault = true;
-    overlays = [ (self: super: {
-      glibc = super.glibc.overrideAttrs (old: {
-        separateDebugInfo = false;
-      });
-      openssl = super.openssl.overrideAttrs (old: {
-        separateDebugInfo = false;
-      });
-      gdk-pixbuf = super.gdk-pixbuf.overrideAttrs (old: {
-        separateDebugInfo = false;
-      });
-      glib = super.glib.overrideAttrs (old: {
-        separateDebugInfo = false;
-      });
-      curl = super.curl.overrideAttrs (old: {
-        separateDebugInfo = false;
-      });
-      valgrind = super.valgrind.overrideAttrs (old: {
-        separateDebugInfo = false;
-      });
-   }) ];
+    overlays = [ (import ./overlay.nix) ];
   });
 in {
   #aflplusplus = pkgs.aflplusplus; # takes too long to build for doing often (LLVM)
